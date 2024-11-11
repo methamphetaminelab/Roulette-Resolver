01e645ac5ea6ca9254a99e44a2a9e970910a1dd5
methamphetaminelab <58376312+methamphetaminelab@users.noreply.github.com>
2024-11-11 04:51:42 +0300

    v1.01

--- main.py
+++ main.py
@@ -9,114 +9,113 @@ class RouletteResolver(ctk.CTk):
 
         self.title('Roulette Resolver')
 
-        screenWidth = self.winfo_screenwidth()
-        screenHeight = self.winfo_screenheight()
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
 
-        windowWidth = 335
-        windowHeight = 305
-        posX = screenWidth - windowWidth
-        posY = 0
+        window_width = 335
+        window_height = 305
+        pos_x = screen_width - window_width
+        pos_y = 0
 
-        self.geometry(f'{windowWidth}x{windowHeight}+{posX}+{posY}')
+        self.geometry(f'{window_width}x{window_height}+{pos_x}+{pos_y}')
 
         self.wm_attributes('-topmost', 1)
         self.attributes('-alpha', 0.8)
         self.overrideredirect(True)
 
-        self.hotRounds = 0
-        self.coldRounds = 0
+        self.hot_rounds = 0
+        self.cold_rounds = 0
 
-        self.entryHot = ctk.CTkEntry(self, placeholder_text="HOT BULLETS", width=160)
-        self.entryHot.grid(row=0, column=0, padx=5, pady=5)
+        self.entry_hot = ctk.CTkEntry(self, placeholder_text='HOT BULLETS', width=160)
+        self.entry_hot.grid(row=0, column=0, padx=5, pady=5)
 
-        self.entryCold = ctk.CTkEntry(self, placeholder_text="COLD BULLETS", width=160)
-        self.entryCold.grid(row=1, column=0, padx=5, pady=5)
+        self.entry_cold = ctk.CTkEntry(self, placeholder_text='COLD BULLETS', width=160)
+        self.entry_cold.grid(row=1, column=0, padx=5, pady=5)
 
-        self.initButton = ctk.CTkButton(self, text="FILL", command=self.fillRounds, width=160)
-        self.initButton.grid(row=2, column=0, padx=5, pady=2)
+        self.init_button = ctk.CTkButton(self, text='FILL', command=self.fill_rounds, width=160)
+        self.init_button.grid(row=2, column=0, padx=5, pady=2)
 
-        self.hotButton = ctk.CTkButton(self, text="HOT", fg_color="red", command=self.removeHot, width=160)
-        self.hotButton.grid(row=3, column=0, padx=5, pady=2)
+        self.hot_button = ctk.CTkButton(self, text='HOT', fg_color='red', command=self.remove_hot, width=160)
+        self.hot_button.grid(row=3, column=0, padx=5, pady=2)
 
-        self.coldButton = ctk.CTkButton(self, text="COLD", fg_color="gray", command=self.removeCold, width=160)
-        self.coldButton.grid(row=4, column=0, padx=5, pady=2)
+        self.cold_button = ctk.CTkButton(self, text='COLD', fg_color='gray', command=self.remove_cold, width=160)
+        self.cold_button.grid(row=4, column=0, padx=5, pady=2)
 
-        self.outputLabel1 = ctk.CTkLabel(self, text="", anchor="w")
-        self.outputLabel1.grid(row=5, column=0, columnspan=3, padx=5, pady=5)
+        self.output_label1 = ctk.CTkLabel(self, text='', anchor='w')
+        self.output_label1.grid(row=5, column=0, columnspan=3, padx=5, pady=5)
 
-        self.outputLabel2 = ctk.CTkLabel(self, text="", anchor="w")
-        self.outputLabel2.grid(row=6, column=0, columnspan=3, padx=5, pady=5)
+        self.output_label2 = ctk.CTkLabel(self, text='', anchor='w')
+        self.output_label2.grid(row=6, column=0, columnspan=3, padx=5, pady=5)
 
-        self.outputLabel3 = ctk.CTkLabel(self, text="", anchor="w")
-        self.outputLabel3.grid(row=7, column=0, columnspan=3, padx=5, pady=5)
+        self.output_label3 = ctk.CTkLabel(self, text='', anchor='w')
+        self.output_label3.grid(row=7, column=0, columnspan=3, padx=5, pady=5)
 
-        self.bulletLabels = []
-        self.bulletStates = []
+        self.bullet_labels = []
+        self.bullet_states = []
 
-    def fillRounds(self):
+    def fill_rounds(self):
         try:
-            self.hotRounds = int(self.entryHot.get())
-            self.coldRounds = int(self.entryCold.get())
-            self.bulletStates = ['unknown'] * (self.hotRounds + self.coldRounds)
-            self.updateOutput()
+            self.hot_rounds = int(self.entry_hot.get())
+            self.cold_rounds = int(self.entry_cold.get())
+            self.bullet_states = ['unknown'] * (self.hot_rounds + self.cold_rounds)
+            self.update_output()
         except ValueError:
-            self.outputLabel.configure(text='PLEASE ENTER CORRECT NUMBERS')
+            self.output_label.configure(text='PLEASE ENTER CORRECT NUMBERS')
 
-    def removeHot(self):
-        if self.hotRounds > 0:
-            self.hotRounds -= 1
-            self.bulletStates.pop(0)
-            self.updateOutput()
+    def remove_hot(self):
+        if self.hot_rounds > 0:
+            self.hot_rounds -= 1
+            self.bullet_states.pop(0)
+            self.update_output()
 
-    def removeCold(self):
-        if self.coldRounds > 0:
-            self.coldRounds -= 1
-            self.bulletStates.pop(0)
-            self.updateOutput()
+    def remove_cold(self):
+        if self.cold_rounds > 0:
+            self.cold_rounds -= 1
+            self.bullet_states.pop(0)
+            self.update_output()
 
-    def updateOutput(self):
-        totalRounds = len(self.bulletStates)
-        chance = (self.bulletStates.count('hot') / totalRounds * 100) if totalRounds > 0 else 0.0
+    def update_output(self):
+        total_rounds = len(self.bullet_states)
+        chance = (self.hot_rounds / total_rounds * 100) if total_rounds > 0 else 0.0
 
-        ot1 = (f'HOT BULLETS: {self.hotRounds}')
+        ot1 = f'HOT BULLETS: {self.hot_rounds}'
+        ot2 = f'COLD BULLETS: {self.cold_rounds}\n'
+        ot3 = f'HOT BULLET CHANCE: {chance:.2f}%'
 
-        ot2 = (f'COLD BULLETS: {self.coldRounds}\n')
-        ot3 = (f'HOT BULLET CHANCE: {chance:.2f}%')
+        self.output_label1.configure(text=ot1)
+        self.output_label2.configure(text=ot2)
+        self.output_label3.configure(text=ot3)
+        self.update_bullets(total_rounds=total_rounds)
 
-        self.outputLabel1.configure(text=ot1)
-        self.outputLabel2.configure(text=ot2)
-        self.outputLabel3.configure(text=ot3)
-        self.updateBullets(totalRounds=totalRounds)
-
-    def updateBullets(self, totalRounds):
-        for label, button_h, button_c in self.bulletLabels:
+    def update_bullets(self, total_rounds):
+        for label, button_h, button_c in self.bullet_labels:
             label.grid_forget()
             button_h.grid_forget()
             button_c.grid_forget()
 
-        self.bulletLabels.clear()
+        self.bullet_labels.clear()
 
-        for round, state in enumerate(self.bulletStates):
-            label_text = (round + 1) if state == "unknown" else (f"{round + 1} HOT" if state == 'hot' else f"{round + 1} COLD")
-            label_color = "transparent" if state == "unknown" else ("red" if state == 'hot' else "gray")
+        for round, state in enumerate(self.bullet_states):
+            label_text = (round + 1) if state == 'unknown' else (f'{round + 1} HOT' if state == 'hot' else f'{round + 1} COLD')
+            label_color = 'transparent' if state == 'unknown' else ('red' if state == 'hot' else 'gray')
 
             label = ctk.CTkLabel(self, text=label_text, fg_color=label_color, width=3)
-            button_h = ctk.CTkButton(self, text="HOT", fg_color="red", command=lambda r=round: self.setHot(r), width=18)
-            button_c = ctk.CTkButton(self, text="COLD", fg_color="gray", command=lambda r=round: self.setCold(r), width=18)
+            button_h = ctk.CTkButton(self, text='HOT', fg_color='red', command=lambda r=round: self.set_hot(r), width=18)
+            button_c = ctk.CTkButton(self, text='COLD', fg_color='gray', command=lambda r=round: self.set_cold(r), width=18)
 
             label.grid(row=round, column=3, padx=5, pady=5)
             button_h.grid(row=round, column=4, padx=5, pady=5)
             button_c.grid(row=round, column=5, padx=5, pady=5)
 
-            self.bulletLabels.append((label, button_h, button_c))
+            self.bullet_labels.append((label, button_h, button_c))
 
-    def setHot(self, round):
-        self.bulletStates[round] = 'hot'
-        self.updateOutput()
+    def set_hot(self, round):
+        self.bullet_states[round] = 'hot'
+        self.update_output()
 
-    def setCold(self, round):
-        self.bulletStates[round] = 'cold'
-        self.updateOutput()
+    def set_cold(self, round):
+        self.bullet_states[round] = 'cold'
+        self.update_output()
 
 if __name__ == '__main__':
     app = RouletteResolver()
